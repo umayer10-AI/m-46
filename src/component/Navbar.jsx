05300feaa9f806bdbfcc6fffa6e477ce39f01c5c
umayer10-AI <mdumayer10@gmail.com>
2026-04-24 19:50:34 +0600

    add

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { HiUserCircle } from 'react-icons/hi';
+import NavLink from './NavLink';
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between items-center max-w-[90%] mx-auto my-5'>
+        <div className='grid grid-cols-3 items-center max-w-[80%] mx-auto my-5'>
             <div>
-
+                <h2></h2>
+                <h2></h2>
+                <h2></h2>
             </div>
-            <div className='flex items-center gap-2'>
-                <h2 className='btn'>Home</h2>
-                <h2 className='btn'>About</h2>
-                <h2 className='btn'>Career</h2>
-            </div>
-            <div className='flex items-center gap-2'>
+            <NavLink></NavLink>
+            <div className='flex items-center gap-2 justify-end'>
                 <h2 className='text-4xl'><HiUserCircle /></h2>
                 <h2 className='btn bg-black text-white font-bold px-8'>Login</h2>
             </div>
