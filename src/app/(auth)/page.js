e0acc7cb9a21e75288fec94fcbe26cc3a49de1c3
umayer10-AI <mdumayer10@gmail.com>
2026-04-24 20:02:20 +0600

    add

--- src/app/(auth)/page.js
+++ src/app/(auth)/page.js
@@ -1,26 +1,12 @@
+import Categories from '@/component/Categories';
 import React from 'react';
 
 const page = () => {
   return (
-    <div>
-      <h2 className='text-3xl font-bold text-center my-10'>Home Page</h2>
-      <button className='btn btn-accent btn-outline'>Hello</button>
-      <button className='btn btn-accent btn-outline'>Hello</button>
-      <button className='btn btn-info'>Hello</button>
-      <div className="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+    <div className='max-w-[90%] mx-auto'>
+      {/* <h2 className='text-3xl font-bold text-center my-10'>Home Page</h2> */}
+      <Categories></Categories>
+      
     </div>
   );
 };
---
+++ src/component/Categories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Categories = () => {
+    return (
+        <div>
+            <h2 className='font-bold text-xl'>All Caterogy</h2>
+            <div className='flex flex-col'>
+                <h2 className='font-semibold text-gray-400 btn active:bg-gray-500 active:text-black'>Breaking News</h2>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Categories;
